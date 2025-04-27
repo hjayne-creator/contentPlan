04d57cfc0fb15de6726ebaab00f9837a13333865
hjayne-creator <127279950+hjayne-creator@users.noreply.github.com>
2025-04-26 18:36:27 -0700

    Create llm_blacklist.py

---
+++ llm_blacklist.py
@@ -0,0 +1,12 @@
+LLM_BLACKLISTED_TERMS = [
+    "revolutionize",
+    "empower",
+    "unleash",
+    "streamline",
+    "enhance",
+    "unlock",
+    "navigating",       
+    "demystifying",
+    "unprecedented",
+    "amplify",
+] 
